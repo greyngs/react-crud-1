c7cc159fd82c577839eda0e28c21c34114d22c6a
Jorge Reyes <reyesjorge0116@gmail.com>
2022-05-04 21:55:54 -0500

    1. Mejora 1 - (errors). Se refactorizo el uso de errors en los formularios ya que la forma en que se hacia estaba desactualizada

---
+++ src/components/AddUserForm.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useForm } from 'react-hook-form';
+
+const AddUserForm = (props) => {
+
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = (data, e) => {
+        console.log(data)
+
+        props.addUser(data)
+        // limpiar datos
+        e.target.reset()
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="name">Name</label>
+            <input type="text" {...register("name", { required: true })}/>
+            {errors.name && errors.name.type === "required" && <span >Name is required</span>}
+            
+
+            <label htmlFor="username">Username</label>
+            <input type="text" {...register("username", { required: true })}
+                className="form-control mb-2"
+            />
+            {errors.username && errors.username.type === "required" && <span >Username is required</span>}
+            <button>Add new user</button>
+        </form>
+    );
+}
+
+export default AddUserForm;
